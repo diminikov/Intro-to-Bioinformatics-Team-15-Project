4c9b737364d8f049ac84cc735045dd73de950f5e
diminikov <diminikov@gmail.com>
2022-09-24 16:03:50 -0400

    Script for loading data into RStudio + supplementary files

---
+++ DataInput.R
@@ -0,0 +1,37 @@
+install.packages("devtools")
+install.packages("tidyverse")
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+BiocManager::install(version = "3.15")
+
+# Install the Human package
+if (!("org.Hs.eg.db" %in% installed.packages())) {
+  # Install this package if it isn't installed yet
+  BiocManager::install("org.Hs.eg.db", update = FALSE)
+}
+
+# Attach the library
+library(org.Hs.eg.db)
+
+# We will need this so we can use the pipe: %>%
+library(magrittr)
+
+expression_df <- readr::read_tsv("GSE183516_Normalized_counts_AT2_pAT2_samples.txt")
+
+# Tuck away the Gene ID column as row names
+expression_df = tibble::column_to_rownames(expression_df,var="...1")
+
+expression_df <- expression_df %>%
+  tibble::rownames_to_column("Gene")
+
+mapped_list <- mapIds(
+  org.Hs.eg.db, # Replace with annotation package for your organism
+  keys = expression_df$Gene,
+  keytype = "ENSEMBL", # Replace with the type of gene identifiers in your data
+  column = "SYMBOL", # The type of gene identifiers you would like to map to
+  multiVals = "list"
+)
+
+
+
