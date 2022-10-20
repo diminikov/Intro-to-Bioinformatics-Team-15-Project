4a2d14575d8553178fcac8a7ac21d77c7d1e123d
terrynelson-tn <tnelson11720@gmail.com>
2022-10-19 21:46:47 -0400

    k_means algorithm added

---
+++ Assignment3/Human_PCA_plot.R
@@ -0,0 +1,31 @@
+library(readr)
+GSE183516_series_matrix <- read_delim("GSE183516_series_matrix.txt", delim = "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE, skip = 28)
+HumanColdata = data.frame(t(GSE183516_series_matrix[1:39,-1]))
+colnames(HumanColdata) = sub("!Sample_", "", GSE183516_series_matrix$X1[1:39])
+rownames(HumanColdata) = unlist(GSE183516_series_matrix[19,-1])
+#HumanColdata = HumanColdata[,-1]
+HumanColdata = HumanColdata[-37:-60,]
+
+
+GSE183516_Normalized_counts_H23_H358_A549_cell_lines <- read_delim("GSE183516_Normalized_counts_H23_H358_A549_cell_lines.txt", delim = "\t", escape_double = FALSE, trim_ws = TRUE)
+HumanCounts<-GSE183516_Normalized_counts_H23_H358_A549_cell_lines[,order(colnames(GSE183516_Normalized_counts_H23_H358_A549_cell_lines))]
+GSE183516_Normalized_counts_H23_H358_A549_cell_lines[ , order(colnames(GSE183516_Normalized_counts_H23_H358_A549_cell_lines))]
+
+HumanCounts = data.frame(GSE183516_Normalized_counts_H23_H358_A549_cell_lines)
+rownames(HumanCounts) = GSE183516_Normalized_counts_H23_H358_A549_cell_lines$...1
+HumanCounts = HumanCounts[,-1]
+
+library("stringr")
+HumanCounts = HumanCounts[,str_sort(names(HumanCounts), numeric = TRUE)]
+
+all(rownames(HumanColdata) %in% colnames(HumanCounts))
+
+
+library("DESeq2")
+humandds <- DESeqDataSetFromMatrix(countData = round(HumanCounts),
+                              colData = HumanColdata,
+                              design = ~ title)
+
+humanvsd <- vst(humandds)
+#rld <- rlog(dds)
+plotPCA(humanvsd, intgroup=c("title"))
--- Assignment3/Part2.R
+++ Assignment3/Part2.R
@@ -1,11 +1,13 @@
 source("C:/Users/tnels/Documents/Intro-to-Bioinformatics-Team-15-Project/Human_PCA_plot.R")
 #order human counts by variation
 var_genes <- apply(HumanCounts, 1, var)
-head(var_genes)
+#head(var_genes)
 #sort by decreasing and get gene names of the top 5000 rows
 select_var <- names(sort(var_genes, decreasing = TRUE))[1:5000]
-head(select_var)
+#head(select_var)
+# use top5k for cluster algorithm 
 top5k <- HumanCounts[select_var,]
-view(top5k)
+#view(top5k)
+
 
 
