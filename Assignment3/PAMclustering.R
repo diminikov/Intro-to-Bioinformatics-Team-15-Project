738f72d7c1d4d9f3e811462b3e26b2108a1c0052
Stephen Smith <stephendsmith22@gmail.com>
2022-10-19 21:11:46 -0400

    Created Pam cluster

--- Assignment3/PAMclustering.R
+++ Assignment3/PAMclustering.R
@@ -1,3 +1,12 @@
 source("C:/Users/Stephen Smith/Documents/Intro-to-Bioinformatics-Team-15-Project/Human_PCA_plot.R")
 
+library(cluster)
+library(factoextra)
+
+pamResult <- pam(top5k, k = 2)
+pamResult
+#pam(x, k, metric="euclidean", stand=FALSE)
+
+
+
 
