1a37293b3fc83864a5b30f06727135c6d290b6de
Stephen Smith <stephendsmith22@gmail.com>
2022-10-19 21:28:03 -0400

    Created graph for PAM cluster

--- Assignment3/PAMclustering.R
+++ Assignment3/PAMclustering.R
@@ -1,9 +1,12 @@
 source("C:/Users/Stephen Smith/Documents/Intro-to-Bioinformatics-Team-15-Project/Human_PCA_plot.R")
-
 library(cluster)
 library(factoextra)
 
-pamResult <- pam(top5k, k = 2)
+scaleddata = scale(top5k)
+fviz_nbclust(scaleddata, pam, method ="silhouette")+theme_minimal()
+
+
+pamResult <- pam(top5k, k = 25)
 pamResult
 #pam(x, k, metric="euclidean", stand=FALSE)
 
