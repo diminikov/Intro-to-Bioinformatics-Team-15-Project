2ee6fd94ac211f48898beac27b9bf24908e6a27c
Stephen Smith <stephendsmith22@gmail.com>
2022-10-23 23:54:14 -0400

    Determined cluster methods for top 10, 100, and 1000 genes

--- Assignment3/Part2.R
+++ Assignment3/Part2.R
@@ -1,4 +1,4 @@
-source("C:/Users/tnels/Documents/Intro-to-Bioinformatics-Team-15-Project/Human_PCA_plot.R")
+source("C:/Users/Stephen Smith/Documents/Intro-to-Bioinformatics-Team-15-Project/Human_PCA_plot.R")
 #order human counts by variation
 var_genes <- apply(HumanCounts, 1, var)
 #head(var_genes)
@@ -9,5 +9,16 @@ select_var <- names(sort(var_genes, decreasing = TRUE))[1:5000]
 top5k <- HumanCounts[select_var,]
 #view(top5k)
 
+#rerunning cluster method with 10, 100, 1000, and 10000 genes
+select_var <- names(sort(var_genes, decreasing = TRUE))[1:10]
+top10 <- HumanCounts[select_var,]
 
+select_var <- names(sort(var_genes, decreasing = TRUE))[1:100]
+top100 <- HumanCounts[select_var,]
+
+select_var <- names(sort(var_genes, decreasing = TRUE))[1:1000]
+top1k <- HumanCounts[select_var,]
+
+select_var <- names(sort(var_genes, decreasing = TRUE))[1:10000]
+top10k <- HumanCounts[select_var,]
 
