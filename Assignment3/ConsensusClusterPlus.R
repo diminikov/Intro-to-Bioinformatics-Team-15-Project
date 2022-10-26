2bfbc5c639a4e0628fb63684118b06c097ba275d
Alex Dorfman <alexdorfman81@gmail.com>
2022-10-26 17:49:44 -0400

    Created heatmap for CCP.

--- Assignment3/ConsensusClusterPlus.R
+++ Assignment3/ConsensusClusterPlus.R
@@ -1,11 +1,34 @@
+
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
-
+library(ClusterR)
+library(cluster)
 BiocManager::install("ConsensusClusterPlus")
 d <- data.matrix(top5k,rownames.force = NA)
 d = sweep(d,1,apply(d, 1, median,na.rm=T))
 library(ConsensusClusterPlus)
+
 title=tempdir()
 results = ConsensusClusterPlus(d,maxK=25,reps=1000,pItem=0.8,pFeature=1,title=title,clusterAlg="hc",distance="pearson",seed=1262118388.71279,plot="png")
+results[[2]][["consensusMatrix"]]]
+#consensusTree - hclust object
+results[[2]][["consensusTree"]]
+hclust(d = as.dist(1 - fm), method = finalLinkage)
+
 icl = calcICL(results,title=title,plot="png")
-icl[["clusterConsensus"]]
+results <- data.matrix(icl[["clusterConsensus"]])
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("ComplexHeatmap")
+
+#convert out cluster data frame into a matrix
+ResultMatrixCCP <- data.matrix(results)
+view(ResultMatrixCCP)
+
+heatmap(ResultMatrixCCP)
+
+
+
+
